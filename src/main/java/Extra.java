d96b85805305b6f4605d2ed5b59bd0507d25ba3e
merunno <merunno92@gmail.com>
2022-06-13 17:05:30 +0900

    first commit

---
+++ src/main/java/Extra.java
@@ -0,0 +1,18 @@
+public class Extra {
+
+    public static void main(String[] args) {
+        int apple = 500;
+        int eatApple = 0;
+
+        System.out.println("リンゴの数は" + apple + "個です。");
+
+        eatApple++;
+
+        System.out.println(eatApple + "個食べていいですよ");
+
+        apple--;
+
+        System.out.println("残りのリンゴの数は" + apple + "個です。");
+    }
+
+}
